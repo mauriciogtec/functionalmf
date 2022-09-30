efcc12ca726ce210a1174264a063360e6fa9ab5a
Mauricio Tec <mauriciogtec@utexas.edu>
2022-09-30 06:52:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     license="GNU GPLv3",
     packages=['functionalmf'],
     ext_modules=extensions,
-    install_requires=['numpy', 'scipy', 'matplotlib', 'pypolyagamma', 'seaborn', 'scikit-sparse', 'SharedArray'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'seaborn', 'scikit-sparse', 'SharedArray'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python'
